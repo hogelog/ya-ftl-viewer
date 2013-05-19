5763db8353f88286929b50f73e2cae898dfa8006
hogelog <konbu.komuro@gmail.com>
2013-05-19 19:55:20 +0900

    json import feature

--- src/main/java/org/hogel/JsonData.java
+++ src/main/java/org/hogel/JsonData.java
@@ -7,23 +7,47 @@ import net.arnx.jsonic.JSON;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class JsonData {
     private final Map<String, Object> data;
 
-    public JsonData(Map<String, Object> map) {
-        data = (Map<String, Object>) map.get("data");
+    public JsonData(File file, Map<String, Object> map) throws IOException {
+        data = new HashMap<String, Object>();
+        List<String> importPaths = (List<String>) map.get("import");
+        if (importPaths != null) {
+            File baseDir = file.getParentFile();
+            importData(baseDir, importPaths);
+        }
+        data.putAll((Map<String, Object>) map.get("data"));
+    }
+
+    private void importData(File baseDir, List<String> importPaths) throws IOException {
+        for (String importPath : importPaths) {
+            File importFile = new File(baseDir, importPath);
+            Optional<JsonData> importJsonData = JsonData.load(importFile);
+            if (!importJsonData.isPresent()) {
+                continue;
+            }
+            Map<String, Object> importData = importJsonData.get().getData();
+            data.putAll(importData);
+        }
     }
 
     public static Optional<JsonData> load(String path) throws IOException {
         File file = new File(path);
+        return load(file);
+    }
+
+    public static Optional<JsonData> load(File file) throws IOException {
         if (!file.exists()) {
             return Optional.absent();
         }
         String text = Files.toString(file, Charsets.UTF_8);
         Map<String, Object> map = JSON.decode(text);
-        return Optional.of(new JsonData(map));
+        return Optional.of(new JsonData(file, map));
     }
 
     public Map<String, Object> getData() {
--- src/test/java/org/hogel/JsonDataTest.java
+++ src/test/java/org/hogel/JsonDataTest.java
@@ -9,9 +9,15 @@ import static org.hamcrest.CoreMatchers.is;
 
 public class JsonDataTest {
     @Test
-    public void hoge() throws IOException {
+    public void readJson() throws IOException {
         JsonData data = JsonData.load("src/test/resources/web/hoge.json").get();
         assertThat((String) data.getData().get("name"), is("hogelog"));
+    }
+
+    @Test
+    public void importData() throws IOException {
+        JsonData data = JsonData.load("src/test/resources/web/hoge.json").get();
+        assertThat(Number.class.cast(data.getData().get("id")).intValue(), is(10));
     }
 
 }
