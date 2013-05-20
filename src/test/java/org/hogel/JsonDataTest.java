9318c17e9dcb1e563343974094147680a33dc853
hogelog <konbu.komuro@gmail.com>
2013-05-21 02:05:45 +0900

    fix test

--- src/test/java/org/hogel/JsonDataTest.java
+++ src/test/java/org/hogel/JsonDataTest.java
@@ -10,13 +10,13 @@ import static org.hamcrest.CoreMatchers.is;
 public class JsonDataTest {
     @Test
     public void readJson() throws IOException {
-        JsonData data = JsonData.load("src/test/resources/web/hoge.json").get();
+        JsonData data = JsonData.load("src/test/resources/web/data/hoge.json").get();
         assertThat((String) data.getData().get("name"), is("hogelog"));
     }
 
     @Test
     public void importData() throws IOException {
-        JsonData data = JsonData.load("src/test/resources/web/hoge.json").get();
+        JsonData data = JsonData.load("src/test/resources/web/data/hoge.json").get();
         assertThat(Number.class.cast(data.getData().get("id")).intValue(), is(10));
     }
 
